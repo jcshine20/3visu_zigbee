ebe777008b91e318a23f7e72a863e9d54fd6b2f2
kinimodbest <hansen.dominik@web.de>
2022-01-25 15:09:22 +0100

    compressed

--- main.py
+++ main.py
@@ -26,24 +26,25 @@ toolbar = Text("1 = Ende, 2 = Neustart", y=.5, x=-.25)
 Sky(texture='sky')
 
 #Spieler evtl collider sphere??
-player = Entity(model='sphere', color=color.black, scale=(1,1,1), rotation=(0,180,0), collider='box')
+player = Entity(model='Schiff', color=color.orange, scale=(.3,.3,.3), rotation=(0,180,0), collider="box")
 player.collider.visible = True
-lebend = True
 
 
-positions = [10, 0, -10, -20, -60, -100]
 
-for i in range(len(positions)):
-    asteroid = Entity(model='Asteroid', color=color.gray, collider="box", scale=(.5, .5, .5), rotation=(0, 0, 0), position=(random.randint(-6, 6 ), random.randint(-6, 6), positions[0]))
+positions = [15, 0, -10, -200, -60, -100]
+
+#for i in range(len(positions)):
+    #asteroid = Entity(model='Asteroid', color=color.gray, collider="sphere", scale=(.5, .5, .5), rotation=(0, 0, 0), position=(random.randint(-6, 6 ), random.randint(-6, 6), positions[i]))
 
 
-asteroid0 = Entity(model='Asteroid1', collider="box", scale=(.5, .5, .5), rotation=(0, 0, 0), position=(random.randint(-6, 6), random.randint(-6, 6), positions[0]))
-asteroid1 = Entity(model='Asteroid', color=color.gray, collider="sphere", scale=(.5, .5, .5), rotation=(0, 0, 0), position=(random.randint(-6, 6), random.randint(-6, 6), positions[1]))
+asteroid0 = Entity(model='Asteroid1', color=color.gray, collider="box", scale=(.5, .5, .5), rotation=(0, 0, 0), position=(random.randint(-6, 6), random.randint(-6, 6), positions[0]))
+asteroid1 = Entity(model='Asteroid', color=color.gray, collider="box", scale=(.5, .5, .5), rotation=(0, 0, 0), position=(random.randint(-6, 6), random.randint(-6, 6), positions[1]))
 asteroid2 = Entity(model='Asteroid', color=color.gray, collider="box", scale=(.5, .5, .5), rotation=(0, 0, 0), position=(random.randint(-6, 6), random.randint(-6, 6), positions[2]))
 asteroid3 = Entity(model='Asteroid', color=color.gray, collider="box", scale=(.5, .5, .5), rotation=(0, 0, 0), position=(random.randint(-6, 6), random.randint(-6, 6), positions[3]))
 asteroid4 = Entity(model='Asteroid', color=color.gray, collider="box", scale=(.5, .5, .5), rotation=(0, 0, 0), position=(random.randint(-6, 6), random.randint(-6, 6), positions[4]))
 asteroid5 = Entity(model='Asteroid', color=color.gray, collider="box", scale=(.5, .5, .5), rotation=(0, 0, 0), position=(random.randint(-6, 6), random.randint(-6, 6), positions[5]))
 
+#asteroid.collider.visible = True
 asteroid0.collider.visible = True
 asteroid1.collider.visible = True
 asteroid2.collider.visible = True
@@ -51,6 +52,7 @@ asteroid3.collider.visible = True
 asteroid4.collider.visible = True
 asteroid5.collider.visible = True
 
+#1 uns 2
 def input (key):
     if held_keys['space']:
         print(player.rotation_x)
@@ -60,8 +62,7 @@ def input (key):
         player.visible = True
         player.setPos(0,0,0)
         player.collider.setScale(1)
-        global lebend
-        lebend = True
+
 
 def update():
     #steuerung Spieler
@@ -94,22 +95,22 @@ def update():
 
 
     #Asteroiden
-    for i in range(len(positions)):
+    #for i in range(len(positions)):
+        #asteroid.z -= wert * time.dt
+        #asteroid.rotation_x -= 5
+        #endpos = 25
+        #if asteroid.z <= -2:
+            #asteroid.setPos(random.randint(-6, 6), random.randint(-6, 6), endpos)
 
-        asteroid.z -= wert * time.dt
-        asteroid.rotation_x -= 5
-        endpos = 25
-        if asteroid.z <= -2:
-            asteroid.setPos(random.randint(-6, 6), random.randint(-6, 6), endpos)
     asteroid0.z -= wert * time.dt
     asteroid0.rotation_x -= 5
-    endpos = 25
+    endpos = 30
     if asteroid0.z <= -2:
         asteroid0.setPos(random.randint(-6, 6), random.randint(-6, 6), endpos)
     asteroid1.z -= wert * time.dt
     asteroid1.rotation_x -= 5
     asteroid1.rotation_y -= 4
-    endpos = 25
+    endpos = 35
     if asteroid1.z <= -2:
         asteroid1.setPos(random.randint(-6,6), random.randint(-6,6), endpos)
     asteroid2.z -= wert * time.dt
@@ -121,38 +122,62 @@ def update():
     asteroid3.z -= wert * time.dt
     asteroid3.rotation_x -= 5
     asteroid3.rotation_y += 2
-    endpos = 25
+    endpos = 45
     if asteroid3.z <= -2:
         asteroid3.setPos(random.randint(-6, 6), random.randint(-6, 6), endpos)
     asteroid4.z -= wert * time.dt
     asteroid4.rotation_x -= 5
     asteroid4.rotation_y += 6
-    endpos = 25
+    endpos = 38
     if asteroid4.z <= -2:
         asteroid4.setPos(random.randint(-6, 6), random.randint(-6, 6), endpos)
     asteroid5.z -= wert * time.dt
     asteroid5.rotation_x -= 5
     asteroid5.rotation_y += 3
-    endpos = 25
+    endpos = 28
     if asteroid5.z <= -2:
         asteroid5.setPos(random.randint(-6, 6), random.randint(-6, 6), endpos)
 
-    #kollision
-    kollision = player.intersects()
-    if kollision.hit:
-        global lebend
-        while lebend == True:
-            print("kollision")
+    #kollision Spieler
+    lebend = 1
+    kollisionSp = player.intersects()
+    if kollisionSp.hit:
+        lebend -= 1
+        print("kollision", kollisionSp.world_point)
+        print(lebend)
+        while lebend <= 0:
             player.visible = False
-            player.collider.setScale(0)
-            explosion = Entity(model='sphere', color=color.red, scale=1, position=kollision.world_point)
+            explosion = Entity(model='sphere', color=color.red, scale=1, position=kollisionSp.world_point)
             explosion.animate_scale(3,.3)
             destroy(explosion, delay=.3)
-            lebend = False
+
+
+
+    #Kollision Asteroid
+    kollisionAs0 = asteroid0.intersects()
+    if kollisionAs0.hit:
+        asteroid0.setPos(random.randint(-6, 6), random.randint(-6, 6), endpos)
+    kollisionAs1 = asteroid1.intersects()
+    if kollisionAs1.hit:
+        asteroid1.setPos(random.randint(-6, 6), random.randint(-6, 6), endpos)
+    kollisionAs2 = asteroid2.intersects()
+    if kollisionAs2.hit:
+        asteroid2.setPos(random.randint(-6, 6), random.randint(-6, 6), endpos)
+    kollisionAs3 = asteroid3.intersects()
+    if kollisionAs3.hit:
+        asteroid3.setPos(random.randint(-6, 6), random.randint(-6, 6), endpos)
+    kollisionAs4 = asteroid4.intersects()
+    if kollisionAs4.hit:
+        asteroid4.setPos(random.randint(-6, 6), random.randint(-6, 6), endpos)
+    kollisionAs5 = asteroid5.intersects()
+    if kollisionAs5.hit:
+        asteroid5.setPos(random.randint(-6, 6), random.randint(-6, 6), endpos)
 
 
     #Punktzahl
-    #punktzahl
+    Punktzahl = Text("Punktzahl:",y=.5,x=.6 )
+
+
 
 
 
