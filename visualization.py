d1f3d89de5e699c22e2f66c1df1516018467aae6
phil-phm <73504529+phil-phm@users.noreply.github.com>
2022-01-30 17:17:46 +0100

    add quaternion visualization

--- visualization.py
+++ visualization.py
@@ -5,18 +5,13 @@ from ComputationEulerAngles import *
 toRad = 2 * np.pi / 360
 toDeg = 1 / toRad
 
-# scene.range = 5
-# scene.forward = vector(-1, -1, -1)
-# scene.width = 600
-# scene.height = 600
-# scene.title = "<b>3D Visualization Euler Angles</b>"
 
 def createCanvas():
     return canvas(range = 5, forward = vector(-1,-1,-1), width = 600, height = 600)
 
 def createEulerGraph():
     gd = graph(title="3D Visualization", xtitle="<b>Time in s</b>", ytitle="Angels in Degrees",
-               foreground=color.black, background=color.white, fast=False)
+               foreground=color.black, background=color.white, fast=False, scroll = True, xmin=0, xmax=5)
     pitchCurve = gdots(color=color.red, label="pitch")
     rollCurve = gdots(color=color.green, label="roll")
     yawCurve = gdots(color=color.blue, label="yaw")
@@ -73,10 +68,112 @@ def sensorVis():
     return allObj
 
 
+def visualizeQuaternion(row, frontArrowQuat, upArrowQuat, sideArrowQuat,
+                        quaternionObj):
+    """
+    This function updates the 3D visualization of the sensor according to the data received
+    :param row: values of current quaternion
+    :param curves: list of curves for the 4 values of one quaternion
+    :param frontArrowQuat:
+    :param upArrowQuat:
+    :param sideArrowQuat:
+    :param quaternionObj:
+    :return:
+    """
+
+    q0 = float(row[1])
+    q1 = float(row[2])
+    q2 = float(row[3])
+    q3 = float(row[4])
+
+    # Transformation quaternions into euler angels
+    roll = -math.atan2(2 * (q0 * q1 + q2 * q3), 1 - 2 * (q1 * q1 + q2 * q2))
+    pitch = math.asin(2 * (q0 * q2 - q3 * q1))
+    yaw = -math.atan2(2 * (q0 * q3 + q1 * q2), 1 - 2 * (q2 * q2 + q3 * q3))  # - np.pi/2
+
+    # Update Graph
+    # curves[0].plot((q0, row[5]))
+    # curves[1].plot((q1, row[5]))
+    # curves[2].plot((q2, row[5]))
+    # curves[3].plot((q3, row[5]))
+
+    frontVector, upVectorRotated, sideVectorRotated = computeAxis(pitch, roll, yaw)
+
+    frontArrowQuat.axis = frontVector
+    frontArrowQuat.length = 2
+    sideArrowQuat.axis = sideVectorRotated
+    sideArrowQuat.length = 2
+    upArrowQuat.axis = upVectorRotated
+    upArrowQuat.length = 2
+
+    quaternionObj.axis = frontVector
+    quaternionObj.up = upVectorRotated
+
+def visualizeEuler(row, frontArrowEuler, upArrowEuler, sideArrowEuler, eulerObj, deltaTime, phi, theta):
+    """
+    Computation of Euler Angles with help of sensor data
+    """
+    roll = computeRollAngle(accy=row[1], accz=row[2], gyrox=row[3], phiOld=phi, dt=0.050) * toRad
+    pitch = computePitchAngle(accx=row[0], accz=row[2], gyroy=row[4], thetaOld=theta, dt=0.050) * toRad
+    yaw = computeYawAngle(theta=pitch, phi=roll, magx=row[6], magy=row[7], magz=row[8]) * toRad
+
+    phi = roll * toDeg
+    theta = pitch * toDeg
+    # print(pitch * toDeg, roll * toDeg, yaw * toDeg)
+
+    '''
+    Update Graphics
+    '''
+    # curves[0].plot(pos=(deltaTime, roll * toDeg))
+    # curves[1].plot(pos=(deltaTime, pitch * toDeg))
+    # curves[2].plot(pos=(deltaTime, yaw * toDeg))
+
+    frontVector, upVectorRotated, sideVectorRotated = computeAxis(pitch, roll, yaw)
+
+    frontArrowEuler.axis = frontVector
+    frontArrowEuler.length = 2
+    sideArrowEuler.axis = sideVectorRotated
+    sideArrowEuler.length = 2
+    upArrowEuler.axis = upVectorRotated
+    upArrowEuler.length = 2
+
+    eulerObj.axis = frontVector
+    eulerObj.up = upVectorRotated
+
+    deltaTime = deltaTime + 0.05
+
+    return phi, theta, deltaTime
+
+
+def computeAxis(pitch, roll, yaw):
+    """
+    This functions computes the new alignment body-fixed coordinate system of the sensor
+    :param pitch: pitch angle in radians
+    :param roll: roll angle in radians
+    :param yaw: yaw angle in radians
+    :return: vectors describing the new alignment of the body-fixed coordinate system axis
+    """
+
+    """
+     Computation of new x,y,z coordinates of sensor with help of euler angles
+    """
+    frontVector = vector(cos(yaw) * cos(pitch), sin(pitch), sin(yaw) * cos(pitch))
+    y = vector(0, 1, 0)
+    sideVector = cross(frontVector, y)
+    upVector = cross(sideVector, frontVector)
+
+    '''
+    Computation of y- and z-Axis because they are influenced by roll rotations
+    '''
+    upVectorRotated = upVector * cos(roll) + cross(frontVector, upVector) * sin(roll)
+    sideVectorRotated = cross(frontVector, upVectorRotated)
+
+    return frontVector, upVectorRotated, sideVectorRotated
+
 def main():
-    '''Build up Graph for Euler Angels'''
-    gd, pitchCurve, rollCurve, yawCurve = createEulerGraph()
-    gd.align = "right"
+    # '''Build up Graph for Euler Angels'''
+    # gd, pitchCurve, rollCurve, yawCurve = createEulerGraph()
+    # gd.align = "right"
 
     '''Build up Scene for Euler Angels'''
     scene = createCanvas()
@@ -96,8 +193,6 @@ def main():
     quaternionObj = sensorVis()
 
 
-    # gd, pitchCurve, rollCurve, yawCurve = createEulerGraph()
-
 
     theta = 0
     phi = 0
@@ -112,50 +207,19 @@ def main():
 
     while True:
         for i in range(len(list)):
-            """
-            Computation of Euler Angles with help of sensor data
-            """
-            row = list[i].split(",")
-            roll = computeRollAngle(accy=row[1], accz=row[2], gyrox=row[3], phiOld=phi, dt=0.050) * toRad
-            pitch = computePitchAngle(accx=row[0], accz=row[2], gyroy=row[4], thetaOld=theta, dt=0.050) * toRad
-            yaw = computeYawAngle(theta=pitch, phi=roll, magx=row[6], magy=row[7], magz=row[8]) * toRad
-            theta = pitch * toDeg
-            phi = roll * toDeg
-            # print(pitch * toDeg, roll * toDeg, yaw * toDeg)
-
-            '''
-            Update Graphics
-            '''
-            pitchCurve.plot(pos=(deltaTime, theta))
-            rollCurve.plot(pos=(deltaTime, phi))
-            yawCurve.plot(pos=(deltaTime, yaw * toDeg))
-
             rate(20)
-            """
-            Computation of new x,y,z coordinates of sensor with help of euler angles
-            """
-            frontVector = vector(cos(yaw) * cos(pitch), sin(pitch), sin(yaw) * cos(pitch))
-            y = vector(0, 1, 0)
-            sideVector = cross(frontVector, y)
-            upVector = cross(sideVector, frontVector)
+            row = list[i].split(",")
+            # print(row)
+            if True:
+                phi, theta, deltaTime = visualizeEuler(row, frontArrowEuler, upArrowEuler,
+                                                       sideArrowEuler, eulerObj, deltaTime, phi, theta)
+            else:
+                try:
+                    visualizeQuaternion(row, frontArrowQuat, upArrowQuat, sideArrowQuat,
+                                        quaternionObj)
+                except:
+                    pass
 
-            '''
-            Computation of y- and z-Axis because they are influenced by roll rotations
-            '''
-            upVectorRotated = upVector * cos(roll) + cross(frontVector, upVector) * sin(roll)
-            sideVectorRotated = cross(frontVector, upVectorRotated)
-
-            frontArrowEuler.axis = frontVector
-            frontArrowEuler.length = 2
-            sideArrowEuler.axis = sideVectorRotated
-            sideArrowEuler.length = 2
-            upArrowEuler.axis = upVectorRotated
-            upArrowEuler.length = 2
-
-            eulerObj.axis = frontVector
-            eulerObj.up = upVectorRotated
-
-            deltaTime = deltaTime + 0.05
 
 if __name__ == "__main__":
     main()
