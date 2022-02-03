1214140bb11a72f62e3b377ee17963188cc7ecb4
kinimodbest <hansen.dominik@web.de>
2022-02-03 18:43:31 +0100

    buxfix

--- main.py
+++ main.py
@@ -28,18 +28,30 @@ Sky(texture='sky')
 player = Entity(model='Schiff', color=color.dark_gray, scale=(.2, .2, .2), rotation=(0, 180, 0), collider="box")
 player.collider.visible = False
 
-positions = [15, 0, -10, -200, -60, -100, 20, 30]
+positions = [15, 0, -10, -200, -60, -100]
 positionsH = [4,5,6]
 asteroids = []
 herzen  =[]
-lebend = 3
+lebend = 2
 Punktzahl = 0
+highscore = 0
 for i in range(len(positions)):
-    asteroid = Entity(model='Asteroid', color=color.light_gray,
+    asteroid = Entity(model='Asteroid', color=color.gray,
                       collider="box", scale=(.5, .5, .5), rotation=(0, 0, 0),
+                      shader=lit_with_shadows_shader,
                       position=(random.randint(-6, 6), random.randint(-5, 5), positions[i]))
     asteroids.append(asteroid)
     asteroid.collider.visible = False
+
+'''Points, Health Bar, HighScore'''
+points_text = Text(text=f"Punktzahl: {Punktzahl}", y=.5, x=.6, eternal=True, ignore=False, i=0)
+highscore_text = Text(text=f"Highscore: {highscore}", y=.47, x=.6, eternal=True, ignore=False, i=0)
+
+'''Leben'''
+for i in range(len(positionsH)):
+    herz = Entity(model='Sphere', color=color.red,
+                  scale=(.3, .3, .3), position=(positionsH[i], -3, -3))
+    herzen.append(herz)
 
 
 # 1 und 2
@@ -49,7 +61,10 @@ def input(key):
     if held_keys['1']:
         quit()
     if held_keys['2']:
-        lebend = 3
+        global lebend
+        global Punktzahl
+        lebend = 2
+        Punktzahl = 0
         player.visible = True
         player.setPos(0, 0, 0)
         player.collider.setScale(1)
@@ -57,13 +72,44 @@ def input(key):
 
 def update():
     # steuerung Spieler
-    global lebend, herz2, herz1, herz0
+    global lebend
     global Punktzahl
+    global highscore
+    global roll
+    global pitch
     wertSteuer = 4
     wert = 10
     max = 15
+
+    # if increment < len(list):
+    #     row = list[increment].split(",")
+    #     increment += 1
+    #
+    #     roll = computeRollAngle(accy=row[1], accz=row[2], gyrox=row[3], phiOld=roll, dt=0.050)
+    #     player.rotation_z =  roll
+    #
+    #     pitch = computePitchAngle(accx=row[0], accz=row[2], gyroy=row[4], thetaOld=pitch, dt=0.050)
+    #     player.rotation_x = pitch
+    #
+    #     if roll >= 20:
+    #         player.x += -wertSteuer * time.dt
+    #     if roll <=-20:
+    #         player.x += wertSteuer * time.dt
+    #     if pitch >= 20:
+    #         player.y += wertSteuer * time.dt
+    #     if pitch <= -20:
+    #         player.y += -wertSteuer * time.dt
+    #
+    # else:
+    #     increment = 0
+    #     player.setPos(0,0,0)
+    #     print("List durch")
+    #
+    #
+    #
     player.rotation_x = 0
     player.rotation_z = 0
+
 
     if held_keys['w']:
         player.y += wertSteuer * time.dt
@@ -86,41 +132,33 @@ def update():
     while player.x <= -6:
         player.x += 1
 
-
     for asteroid in asteroids:
         asteroid.z -= wert * time.dt
         asteroid.rotation_x -= random.randint(3, 6)
         asteroid.rotation_y -= random.randint(2, 8)
         if asteroid.z <= -2:
             asteroid.setPos(x=random.randint(-6, 6), y=random.randint(-5, 5), z=random.randint(30, 40))
-            Punktzahl += 1
+            if lebend > 0:
+                Punktzahl += 1
+            else:
+                Punktzahl = Punktzahl
 
     # kollision Spieler
 
     kollisionSp = player.intersects()
     if kollisionSp.hit:
         lebend -= 1
-
         print("kollision", kollisionSp.world_point)
         print(lebend)
         if lebend <= 0:
             player.visible = False
-            explosion = Entity(model='sphere', color=color.red, scale=1, position=kollisionSp.world_point, shader = False)
+            explosion = Entity(model='sphere', color=color.red, scale=1, position=kollisionSp.world_point)
             explosion.animate_scale(3, .3)
             destroy(explosion, delay=.3)
+            if highscore < Punktzahl:
+                highscore = Punktzahl
 
-    for asteroid in asteroids:
-        kollisionAs = asteroid.intersects()
-        if kollisionAs.hit:
-            asteroid.setPos(random.randint(-6, 6), random.randint(-6, 6), random.randint(30, 40))
-
-
-    #Herzen
-
-    for i in range(len(positionsH)):
-        herz = Entity(model='Sphere', color=color.red,
-                      scale=(.3, .3, .3), position=(positionsH[i], -3, -3))
-        herzen.append(herz)
+    '''Leben wird weniger'''
     if lebend == 2:
         herzen[2].visible = False
     if lebend == 1:
@@ -128,20 +166,21 @@ def update():
     if lebend == 0:
         herzen[0].visible = False
 
+    for asteroid in asteroids:
+        kollisionAs = asteroid.intersects()
+        if kollisionAs.hit:
+            asteroid.setPos(random.randint(-6, 6), random.randint(-6, 6), random.randint(30, 40))
+        # if lebend <= 0:
+        #     player.visible = False
+        #     explosion = Entity(model='sphere', color=color.red, scale=1, position=kollisionSp.world_point)
+        #     explosion.animate_scale(3,.3)
+        #     destroy(explosion, delay=.3)
 
+    destroy(points_text)
+    points_text.text = f"Punktzahl: {Punktzahl}"
 
-
-    #Punktzahl
-    #Punktzahl = Text("Punktzahl:",y=.5,x=.6 )
-
-    # Punktzahl
-
-    Punktzahl_text = Text(text=f"Punktzahl: {Punktzahl}", y=.5, x=.6, eternal=True, ignore=False,i=0)
-    #text_entity = Text('text' + str(Punktzahl))
-
-    # wp = WindowPanel(content=text_entity, popup=True, enabled=False)
-    # to update the text
-
+    destroy(highscore_text)
+    highscore_text.text = f"Highscore: {highscore}"
 
 
 app.run()
