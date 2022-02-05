11d9bd87207f723efdcc9acbd903e3042f196217
phil-phm <73504529+phil-phm@users.noreply.github.com>
2022-02-05 01:01:18 +0100

    add new font style

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ class Asteroid(Entity):  # Klasse Asteroid
             model='Asteroid', collider='Asteroid', scale=(1, 1, 1),
         )
 
+class Explosion(Entity):
+    def __init__(self):
+        super.__init__(
+
+        )
+
 
 app = Ursina()  # Initialisierung Ursina
 
@@ -43,9 +49,10 @@ for i in range(len(positions)):
     asteroids.append(asteroid)
     asteroid.collider.visible = False
 
-'''Points, Health Bar, HighScore'''
-points_text = Text(text=f"Punktzahl: {Punktzahl}", y=.5, x=.6, eternal=True, ignore=False, i=0)
-highscore_text = Text(text=f"Highscore: {highscore}", y=.47, x=.6, eternal=True, ignore=False, i=0)
+'''Fonts, Points, Health Bar, HighScore'''
+gamefont = 'fonts/Pixeboy-z8XGD.ttf'
+points_text = Text(text=f"Punktzahl: {Punktzahl}", y=.5, x=.6, scale = 1.5, eternal=True, ignore=False, i=0, font = gamefont)
+highscore_text = Text(text=f"Highscore: {highscore}", y=.47, x=.6, scale = 1.5, eternal=True, ignore=False, i=0, font = gamefont)
 
 '''Leben'''
 for i in range(len(positionsH)):
